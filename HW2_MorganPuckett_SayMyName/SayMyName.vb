311eb189963565cdc3fb23d46444bcdbd8f3d8b1
Morgan Puckett <morganpuckett02@gmail.com>
2022-01-22 12:42:22 -0700

    Create funtioning code

--- HW2_MorganPuckett_SayMyName/SayMyName.vb
+++ HW2_MorganPuckett_SayMyName/SayMyName.vb
@@ -1,12 +1,18 @@
-﻿Module SayMyName
-    Option Explicit On
-    Option Strict On
+﻿'Morgan Puckett
+'RCET0265
+'Spring 2022
+'Say my name 
+'https://github.com/Morgan-Puckett/HW2_MorganPuckett_SayMyName.git
+
+Option Explicit On
+Option Strict On
+Module SayMyName
     Sub Main()
         Dim response As String
         Console.WriteLine("Hello, can I get your name please?")
         response = Console.ReadLine()
-
-
+        Console.WriteLine($"So {response}, it looks like you're not on the list... I'm going to have to ask you to leave.")
+        Console.ReadLine()
     End Sub
 
 End Module
