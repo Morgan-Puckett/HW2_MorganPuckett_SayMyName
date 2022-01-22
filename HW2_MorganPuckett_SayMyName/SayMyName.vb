7c399cc63b7f3ea0e6c3ccedaa11dace81963cc0
Morgan Puckett <morganpuckett02@gmail.com>
2022-01-22 12:34:14 -0700

    Add project files.

---
+++ HW2_MorganPuckett_SayMyName/SayMyName.vb
@@ -0,0 +1,12 @@
+﻿Module SayMyName
+    Option Explicit On
+    Option Strict On
+    Sub Main()
+        Dim response As String
+        Console.WriteLine("Hello, can I get your name please?")
+        response = Console.ReadLine()
+
+
+    End Sub
+
+End Module
